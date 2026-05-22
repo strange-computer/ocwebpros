d94b050d037615a54405516a2a06004d986dc2b1
Spencer <strangecomputer4u@gmail.com>
2026-05-21 18:16:40 -0700

    set static

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ocwebpros.com',
-  output: 'server',
+  output: 'static',
   adapter: netlify(),
   vite: {
     plugins: [tailwindcss()]
